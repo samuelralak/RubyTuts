7e337c04b23cc794d57feaa223d6a83bf680396b
samuelralak <samuelralak@ovi.com>
2014-01-15 01:12:54 +0300

    added association and depaendant option to comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
-	has_many :comments
+	
+	#dependent option deletes a post and its associated comments
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true,
 		length: {minimum: 5}
 end
